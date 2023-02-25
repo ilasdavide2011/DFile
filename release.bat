c940fa69359bc7b05001393bd719d80e3af77e00
Ilas Davide <ilasdavide2011@gmail.com>
2023-02-25 18:13:45 +0100

    src/Renderer bug fix

---
+++ release.bat
@@ -0,0 +1,1 @@
+g++ -c src/*.cpp src/Renderer/*.cpp -std=c++14 -g -Wall -m64 -I include -I C:\SDL2\include && g++ *.o -o bin\release\main -L C:\SDL2\lib -lmingw32 -lSDL2main -lSDL2 -lSDL2_image && start bin\release\main
